a84b5598ad26451fe6f40402baf4170551949cd0
wangfenwangfen <fenwang@hotmail.fr>
2019-05-20 17:26:25 +0200

    create controller

--- service/src/main/java/application/WebApplication.java
+++ service/src/main/java/application/WebApplication.java
@@ -1,21 +1,13 @@
-package fr.fw.application;
+package application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
 public class WebApplication {
     //seul entree de l'application, cette classe ne doit pas être dans defaut package
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
     }
-
-    @RequestMapping(value = "/test",method = RequestMethod.GET)
-    public String test(){
-        return "test succes";
-    }
 }
+
